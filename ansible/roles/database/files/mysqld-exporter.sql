f0dd83fb81264903ea19c09b2134e44611adfa6e
Mange <mange@pop-os.localdomain>
2021-03-09 11:54:55 +0100

    Kmom04

---
+++ ansible/roles/database/files/mysqld-exporter.sql
@@ -0,0 +1,3 @@
+CREATE USER 'exporter'@'%' IDENTIFIED BY 'passwd' WITH MAX_USER_CONNECTIONS 3;
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
+GRANT SELECT ON performance_schema.* TO 'exporter'@'%';
